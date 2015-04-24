a33ea2a77da5312c601cd82c33e41f818997f14e
Motorkov Anton <a.motorkov@gmail.com>
2015-04-24 12:57:18 +0300

    added class 'get_parameters' for work with configuration file

---
+++ get_parameters.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+#include "get_parameters.h"
+
+using namespace std;
+
+get_parameters::get_parameters (char* config_file_name)
+{
+	config_file = config_file_name;
+	texts_limit = 0;
+}
+
+void get_parameters::get_general_params () 
+{
+	texts_limit = get_int_param("general.texts_limit");
+}
+
+void get_parameters::get_smad_db_params () 
+{
+	smad_db_host = get_param("smad_db.host");
+	smad_db_name = get_param("smad_db.name");
+	smad_db_user = get_param("smad_db.user");
+        smad_db_pass = get_param("smad_db.password");
+}
+
+void get_parameters::get_dict_db_params ()
+{
+	dict_db_host = get_param("dictionary_db.host");
+	dict_db_name = get_param("dictionary_db.name");
+	dict_db_user = get_param("dictionary_db.user");
+	dict_db_pass = get_param("dictionary_db.password");
+	dict_db_encod = get_param("dictionary_db.encoding");
+}
+
+string get_parameters::get_param (string param_name)
+{
+
+	try {
+		config.readFile(config_file);
+	}
+	catch (const FileIOException &fioex) {
+		cerr << "I/O error while reading file." << endl;
+	}
+	catch (const ParseException &pex) {
+		cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << endl;
+	}
+
+	try {
+		return config.lookup(param_name);
+	}
+	catch(const SettingNotFoundException &nfex) {
+		cerr << "No '" << param_name << "' setting in configuration file." << endl;
+	}
+}
+int get_parameters::get_int_param (string param_name)
+{
+
+	try {
+		config.readFile(config_file);
+	}
+	catch (const FileIOException &fioex) {
+		cerr << "I/O error while reading file." << endl;
+	}
+	catch (const ParseException &pex) {
+		cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << endl;
+	}
+
+	try {
+		return config.lookup(param_name);
+	}
+	catch(const SettingNotFoundException &nfex) {
+		cerr << "No '" << param_name << "' setting in configuration file." << endl;
+	}
+}
---
+++ get_parameters.h
@@ -0,0 +1,36 @@
+#include <string>
+#include <libconfig.h++>
+
+using namespace std;
+using namespace libconfig;
+
+class get_parameters
+{
+private:
+	Config config;
+	char* config_file;
+public:
+	// general
+	int texts_limit;
+
+	// smad db
+	string 	smad_db_host,
+		smad_db_name,
+		smad_db_user,
+		smad_db_pass;
+	
+	string 	dict_db_host,
+		dict_db_name,
+		dict_db_user,
+		dict_db_pass,
+		dict_db_encod;
+	
+	get_parameters (char* config_file_name);
+	void get_general_params ();
+	void get_smad_db_params ();
+	void get_dict_db_params ();
+
+private:
+	string get_param (string param_name);
+	int get_int_param (string param_name);
+};

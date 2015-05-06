1afe0507088f7e56ae140ada881ece1b54631bfa
Motorkov Anton <a.motorkov@gmail.com>
2015-05-06 09:10:53 +0300

    Added new parameters for SVM configuration

--- get_parameters.cpp
+++ get_parameters.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <unordered_map>
 
 #include "get_parameters.h"
 
@@ -10,6 +11,8 @@ get_parameters::get_parameters (const char* config_file_name)
 {
 	config_file = config_file_name;
 	texts_limit = 0;
+	svm_types = { {"C_SVC", C_SVC}, {"NU_SVC", NU_SVC} };
+	kernel_types = { {"LINEAR", LINEAR}, {"POLY", POLY}, {"RBF", RBF}, {"SIGMOID", SIGMOID} }; 
 }
 
 void get_parameters::get_general_params () 
@@ -19,6 +22,7 @@ void get_parameters::get_general_params ()
 	number_of_classes = get_int_param("general.number_of_classes");
 	use_tf = get_bool_param("general.calculate_with_tf");
 	v_space_file_name = get_param("general.vector_space_file");
+	model_file_name = get_param("general.model_file");
 }
 
 void get_parameters::get_smad_db_params () 
@@ -36,6 +40,11 @@ void get_parameters::get_dict_db_params ()
 	dict_db_user = get_param("dictionary_db.user");
 	dict_db_pass = get_param("dictionary_db.password");
 	dict_db_encod = get_param("dictionary_db.encoding");
+}
+void get_parameters::get_svm_params()
+{
+	svm_type = svm_types[get_param("svm.svm_type")];
+	kernel_type = kernel_types[get_param("svm.kernel_type")];
 }
 
 string get_parameters::get_param (string param_name)
--- get_parameters.h
+++ get_parameters.h
@@ -1,4 +1,6 @@
 #include <string>
+#include <unordered_map>
+
 #include <libconfig.h++>
 
 using namespace std;
@@ -12,10 +14,12 @@ private:
 public:
 	// general
 	int texts_limit;
-	int n_gramm_size;
+	unsigned int n_gramm_size;
 	int number_of_classes;
 	bool use_tf;
+	
 	string v_space_file_name;
+	string model_file_name;
 
 	// smad db
 	string 	smad_db_host,
@@ -28,11 +32,19 @@ public:
 		dict_db_user,
 		dict_db_pass,
 		dict_db_encod;
+
+	int svm_type,
+	    kernel_type;
+	enum svms {C_SVC, NU_SVC};
+	enum kernels {LINEAR, POLY, RBF, SIGMOID};
+	unordered_map<string, enum svms> svm_types;
+	unordered_map<string, enum kernels> kernel_types;
 	
 	get_parameters (const char* config_file_name);
 	void get_general_params ();
 	void get_smad_db_params ();
 	void get_dict_db_params ();
+	void get_svm_params ();
 
 private:
 	string get_param (string param_name);
